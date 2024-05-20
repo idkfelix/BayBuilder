b3dbf6c35ff4406477b99be7ed081b9c8b509dc3
idkfelix <felix.coulthard@gmail.com>
2024-05-20 10:16:47 +1000

    ratelimit

--- src/routes/[[id]]/+page.server.js
+++ src/routes/[[id]]/+page.server.js
@@ -1,22 +1,32 @@
 import { KV_REST_API_TOKEN, KV_REST_API_URL } from '$env/static/private'
 import { createClient } from "@vercel/kv";
+import { Ratelimit } from "@upstash/ratelimit";
+import { error } from '@sveltejs/kit';
+
+const kv = createClient({url: KV_REST_API_URL, token: KV_REST_API_TOKEN})
+const ratelimit = new Ratelimit({
+  redis: kv,
+  limiter: Ratelimit.slidingWindow(5, "15 s"),
+});
 
 export const load = async ({ params }) => {
-  if(!params.id){
-    return {}
-  }
-  const kv = createClient({url: KV_REST_API_URL, token: KV_REST_API_TOKEN})
+  if(!params.id){return {}}
   const pieces = await kv.get(`pieces:${params.id}`)
 	return {pieces};
 }
 
 export const actions = {
-  default: async ({request}) => {
+  default: async ({request, getClientAddress}) => {
+    const ip = getClientAddress();
+    const rateLimitAttempt = await ratelimit.limit(ip);
+    if (!rateLimitAttempt.success) {
+      error(429, {message: 'Too many requests'})
+    }
+
     const data = await request.formData()
     const pieces = data.get('pieces')
     const id = crypto.randomUUID()
 
-    const kv = createClient({url: KV_REST_API_URL, token: KV_REST_API_TOKEN})
     const res = await kv.set(`pieces:${id}`, pieces)
     return {success: res ? true:false, id}
   }
